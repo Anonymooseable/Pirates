34275d08b5045588bac60ed92578691c2316a601
Bobbster <bobbsterman@gmail.com>
2013-10-21 00:46:54 +0200

    Are you ready? Then say it! (added text and some conditions)

--- Battleship.py
+++ Battleship.py
@@ -1,3 +1,12 @@
-for x in range(1,201):
-    print(x)
+w=0
+while w!=1:
+    print ("Ready?");x=input()
+    if x=="Yea" or x=="Yep" or x=="Yesh" or x=="Yar" or x=="Yup" or x=="yea" or x=="yep" or x=="yesh" or x=="yar" or x=="yup":
+        for x in range(1,201):
+            print(x)
+        w=1
+        print ('Wooooooooooooooooooooot! You did it!')
+    else:
+        print ('Nope...')
+    
     
