e1eed3435d84f8d12bc7148d9a43fa73dbf0502e
Linus <anonymouse2048@gmail.com>
2013-10-22 14:01:05 +0200

    Shorten "yes" check

--- Pirates.py
+++ Pirates.py
@@ -2,12 +2,10 @@
 w=0
 while w!=1:
     print ("Ready?");x=input()
-    if x=="Yea" or x=="Yep" or x=="Yesh" or x=="Yar" or x=="Yup" or x=="yea" or x=="yep" or x=="yesh" or x=="yar" or x=="yup":
+    if x.lower() in ("yea", "yep", "yesh", "yar", "yup"):
         for x in range(1,201):
             print(x)
         w=1
         print ('Wooooooooooooooooooooot! You did it!')
     else:
         print ('Nope...')
-    
-    
