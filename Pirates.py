526495b7e16edc4f0a97caef1cd9ea9e67e63959
Bobbster <bobbsterman@gmail.com>
2013-10-23 20:02:17 +0200

    Attempted to start somethin... (setup event check and board matrix)

--- Pirates.py
+++ Pirates.py
@@ -1,13 +1,34 @@
-# Some comment
-w=0
-while w!=1:
-    print ("Ready?");x=input()
-    if x=="Yea" or x=="Yep" or x=="Yesh" or x=="Yar" or x=="Yup" or x=="yea" or x=="yep" or x=="yesh" or x=="yar" or x=="yup":
-        for x in range(1,201):
-            print(x)
-        w=1
-        print ('Wooooooooooooooooooooot! You did it!')
-    else:
-        print ('Nope...')
-    
-    
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((600, 600))
+pygame.display.set_caption('Yarrrr!!')
+
+board = ((0,1,2,3,4,5),(0,1,2,3,4,5),\
+(0,1,2,3,4,5),(0,1,2,3,4,5),(0,1,2,3,4,5),(0,1,2,3,4,5)) #matrix time!
+y=3
+x=2
+K_LEFT=0;K_RIGHT=0;K_DOWN=0;K_UP=0
+def left():
+    if x!=0:
+        x=x-1
+def right():
+    if x!=5:
+        x=x+1
+def down():
+    if y!=5:
+        y=y+1
+def up():
+    if y!=0:
+        y=y-1    
+cmd = {K_LEFT:left, K_RIGHT:right, \
+K_DOWN:down, K_UP:up} #all possible commands go in here
+running=True
+while running:
+    for user in pygame.event.get():
+        if str(user.type)=='KEYDOWN' and user.key in cmd:
+            cmd[event.key]()
+            print(x,y)
+        cursor_pos=board[y][x]  #remember: rows then columns
+        if str(user.type)=='QUIT':
+            running=False
+pygame.quit()
