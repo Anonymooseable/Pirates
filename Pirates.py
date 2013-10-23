a97b1776c7ed504eb6e3f71181ec294b99ad1294
Linus <anonymouse2048@gmail.com>
2013-10-23 21:39:35 +0200

    Add some spaces and move some vars into class

--- Pirates.py
+++ Pirates.py
@@ -8,13 +8,13 @@ class PiratesGame:
         self.x = 0
         self.y = 0
         def left(self):
-            if self.x!=0:
+            if self.x != 0:
                 self.x -= 1
         def right(self):
-            if self.x!=5:
+            if self.x != 5:
                 self.x += 1
         def down(self):
-            if self.y!=5:
+            if self.y != 5:
                 self.y += 1
         def up(self):
             if self.y!=0:
@@ -25,27 +25,28 @@ class PiratesGame:
             pg.K_DOWN: down,
             pg.K_UP: up
         } # all commands
+        self.board = (
+            (0,1,2,3,4,5),(0,1,2,3,4,5),
+            (0,1,2,3,4,5),(0,1,2,3,4,5),
+            (0,1,2,3,4,5),(0,1,2,3,4,5)
+        ) # matrix time!
+
+        self.running = False
 
     def run(self):
         pygame.init()
         self.screen = pygame.display.set_mode((600, 600))
         pygame.display.set_caption('Yarrrr!!')
 
-        board = (
-            (0,1,2,3,4,5),(0,1,2,3,4,5),
-            (0,1,2,3,4,5),(0,1,2,3,4,5),
-            (0,1,2,3,4,5),(0,1,2,3,4,5)
-        ) # matrix time!
-
-        running=True
-        while running:
+        self.running = True
+        while self.running:
             for user in pygame.event.get():
                 if user.type==pg.KEYDOWN and user.key in self.key_handlers:
                     self.key_handlers[user.key](self)
-                    print(self.x,self.y)
-                    cursor_pos=board[self.y][self.x] # remember: rows then columns
-                if user.type==pygame.QUIT:
-                    running=False
+                    print (self.x, self.y)
+                    cursor_pos = self.board[self.y][self.x] # remember: rows then columns
+                if user.type == pygame.QUIT:
+                    self.running = False
         pygame.quit()
 
 if __name__ == "__main__":
