0b7fb5ffbc395cbc4d308b8a0fc67a7e2e435391
Bobbster <bobbsterman@gmail.com>
2013-10-23 23:46:24 +0200

    Setup cursor position

--- Pirates.py
+++ Pirates.py
@@ -7,6 +7,8 @@ class PiratesGame:
     def __init__(self):
         self.x = 0
         self.y = 0
+        self.x_rect = 0
+        self.y_rect = 0
         def left(self):
             if self.x != 0:
                 self.x -= 1
@@ -25,12 +27,8 @@ class PiratesGame:
             pg.K_DOWN: down,
             pg.K_UP: up
         } # all commands
-        self.board = (
-            (0,1,2,3,4,5),(0,1,2,3,4,5),
-            (0,1,2,3,4,5),(0,1,2,3,4,5),
-            (0,1,2,3,4,5),(0,1,2,3,4,5)
-        ) # matrix time!
-
+        self.x_box = {0:62,1:157,2:252,3:347,4:442,5:537}
+        self.y_box = {0:62,1:157,2:252,3:347,4:442,5:537}
         self.running = False
 
     def run(self):
@@ -39,9 +37,11 @@ class PiratesGame:
         self.running = True
         self.screen = pygame.display.set_mode((600, 600))#25px on sides, 20px 
         pygame.display.set_caption('Yarrrr!!')           #between squares and
-                                                         #75px per square
-        rect=pygame.Rect(75,75,25,25)
-        self.screen.fill((250,250,250),rect)
+                                                         #75px per square and half a square=37px
+        for self.x_rect in [0,95,190,285,380,475]:
+            for self.y_rect in [0,95,190,285,380,475]:
+                rect=pygame.Rect(25+self.x_rect,25+self.y_rect,75,75)  #(x,y,width,height)
+                self.screen.fill((250,250,250),rect)
         pygame.display.flip()
 
         while self.running:
@@ -49,7 +49,8 @@ class PiratesGame:
                 if user.type==pg.KEYDOWN and user.key in self.key_handlers:
                     self.key_handlers[user.key](self)
                     print (self.x, self.y)
-                    cursor_pos = self.board[self.y][self.x] # remember: rows then columns
+                    cursor_pos = [self.x_box[self.x],self.y_box[self.y]]
+                    print (cursor_pos)
                 if user.type == pygame.QUIT:
                     self.running = False
         pygame.quit()
