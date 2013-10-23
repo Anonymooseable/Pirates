27ad18e38acbe7e8dc1d1b41fa6105433fbcdf9d
Linus <anonymouse2048@gmail.com>
2013-10-23 23:50:55 +0200

    Fix drawing of cursor

--- Pirates.py
+++ Pirates.py
@@ -8,20 +8,20 @@ import ship
 
 class PiratesGame:
     def __init__(self):
-        self.x = 0
-        self.y = 0
+        self.cursor_x = 0
+        self.cursor_y = 0
         def left(self):
-            if self.x != 0:
-                self.x -= 1
+            if self.cursor_x != 0:
+                self.cursor_x -= 1
         def right(self):
-            if self.x != 5:
-                self.x += 1
+            if self.cursor_x != 5:
+                self.cursor_x += 1
         def down(self):
-            if self.y != 5:
-                self.y += 1
+            if self.cursor_y != 5:
+                self.cursor_y += 1
         def up(self):
-            if self.y!=0:
-                self.y -= 1
+            if self.cursor_y != 0:
+                self.cursor_y -= 1
         self.key_handlers = {
             pg.K_LEFT: left,
             pg.K_RIGHT: right,
@@ -44,9 +44,9 @@ class PiratesGame:
             for event in pygame.event.get():
                 if event.type==pg.KEYDOWN and event.key in self.key_handlers:
                     self.key_handlers[event.key](self)
-                    pygame.draw.rect((255, 0, 0), (10, 10, self.grid.squares_to_pixels(self.x), self.grid.squares_to_pixels(self.y)))
                 if event.type == pygame.QUIT:
                     self.running = False
+            pygame.draw.rect(self.screen, (255, 0, 0), (self.grid.squares_to_pixels(self.cursor_x), self.grid.squares_to_pixels(self.cursor_y), 10, 10))
             pygame.display.flip()
 
         pygame.quit()
