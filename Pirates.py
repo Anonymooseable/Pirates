4582a7c62a396c9aeeaf23d675e91b9003413b42
Bobbster <bobbsterman@gmail.com>
2013-10-26 18:51:07 +0200

    Added ship generation! (Or at least attempted)

--- Pirates.py
+++ Pirates.py
@@ -2,7 +2,7 @@
 
 import pygame
 import pygame as pg
-
+import random
 class PiratesGame:
     def __init__(self):
         self.x = 0
@@ -29,6 +29,9 @@ class PiratesGame:
         } # all commands
         self.x_box = {0:25,1:120,2:215,3:310,4:405,5:500}
         self.y_box = {0:25,1:120,2:215,3:310,4:405,5:500}
+        self.board = ({0:0,1:0,2:0,3:0,4:5,5:0},{0:0,1:0,2:0,3:0,4:0,5:0},{0:0,1:0,2:0,3:0,4:0,5:0},
+                      {0:0,1:0,2:0,3:0,4:0,5:0},{0:0,1:0,2:0,3:0,4:0,5:0},{0:0,1:0,2:0,3:0,4:0,5:0}) #y=row=choose dict then x=column=choose element in dict
+        
         self.running = False
 
     def run(self):
@@ -39,26 +42,79 @@ class PiratesGame:
         pygame.display.set_caption('Yarrrr!!')
         self.screen.fill((90,50,5))
 
+#Drawing the board
         for self.x_rect in [0,95,190,285,380,475]:
             for self.y_rect in [0,95,190,285,380,475]:
                 rect=pygame.Rect(25+self.x_rect,25+self.y_rect,75,75)  #(x,y,width,height)
                 self.screen.fill((20,70,130),rect)
-        curs_surf=pygame.Surface(self.screen.get_size(),pygame.SRCALPHA) #Creating cursor surface: the cursor shall draw on here.
-        curs_surf.fill((0,0,0,255))
+        copy_screen=pygame.Surface.copy(self.screen)
         pygame.display.flip()
         
         curs = pygame.sprite.Sprite()
         curs.image = pygame.image.load("Cross hair.png").convert_alpha()
         curs.rect = curs.image.get_rect()
 
+#Generating ship positions
+        not_generated=True
+        ship_posx=1
+        ship_posy=1
+        for ship_nb in range(1,4):
+            while not_generated:
+                loop=False
+                x_or_y=random.choice((ship_posx,ship_posy))
+                if x_or_y == ship_posx:
+                    ship_posy=random.randint(0,5)
+                    ship_posx=random.randint(0,5-ship_nb)
+                    if self.board[ship_posy][ship_posx]==0:
+                        self.board[ship_posy][ship_posx]=1
+                        not_generated=False
+                    else:
+                        loop=True
+
+                    for blarg in range(1,ship_nb+1):
+                        x_or_y=x_or_y+blarg
+                        if self.board[ship_posy][ship_posx]==0:
+                            self.board[ship_posy][ship_posx]=1
+                            not_generated=False
+                        else:
+                            loop=True
+                else:
+                    ship_posy=random.randint(0,5-ship_nb)
+                    ship_posx=random.randint(0,5)
+                    if self.board[ship_posy][ship_posx]==0:
+                        self.board[ship_posy][ship_posx]=1
+                        not_generated=False
+                    else:
+                        loop=True
+
+                    for blarg in range(1,ship_nb+1):
+                        x_or_y=x_or_y+blarg
+                        if self.board[ship_posy][ship_posx]==0:
+                            self.board[ship_posy][ship_posx]=1
+                            not_generated=False
+                        else:
+                            loop=True
+                if loop:
+                    not_generated=True
+            
+    
+                            
+                    
+        #check whether this works!!
+        for y in [0,1,2,3,4,5]:
+            for x in [0,1,2,3,4,5]:
+                if self.board[y][x]==1:
+                    print(x,y)
+                    
         while self.running:
             for user in pygame.event.get():
                 if user.type==pg.KEYDOWN and user.key in self.key_handlers:
                     self.key_handlers[user.key](self)
-                    print (self.x, self.y)
-                    curs_surf.fill((0,0,0,255))
-                    curs_surf.blit(curs.image,(self.x_box[self.x],self.y_box[self.y]),curs.rect,pygame.BLEND_RGBA_ADD)
-                    pygame.display.flip()
+                    
+
+                self.screen.blit(copy_screen,(0,0))
+                self.screen.blit(curs.image,(self.x_box[self.x],self.y_box[self.y]))
+                pygame.display.flip()
                     
                 if user.type == pygame.QUIT:
                     self.running = False
